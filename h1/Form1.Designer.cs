1d57d9e7a32a0cb4a024ff85018caeabaefa2a03
judgeyjj <2179067329@qq.com>
2022-04-25 23:08:51 +0800

    Signed-off-by: judgeyjj <2179067329@qq.com>

---
+++ h1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_baidu = new System.Windows.Forms.TextBox();
+            this.textBox_bing = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 34);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(562, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "搜索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_baidu
+            // 
+            this.textBox_baidu.Location = new System.Drawing.Point(86, 133);
+            this.textBox_baidu.Multiline = true;
+            this.textBox_baidu.Name = "textBox_baidu";
+            this.textBox_baidu.Size = new System.Drawing.Size(268, 257);
+            this.textBox_baidu.TabIndex = 2;
+            // 
+            // textBox_bing
+            // 
+            this.textBox_bing.Location = new System.Drawing.Point(390, 133);
+            this.textBox_bing.Multiline = true;
+            this.textBox_bing.Name = "textBox_bing";
+            this.textBox_bing.Size = new System.Drawing.Size(286, 257);
+            this.textBox_bing.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "百度搜索结果";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "bing搜索结果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_bing);
+            this.Controls.Add(this.textBox_baidu);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_baidu;
+        private System.Windows.Forms.TextBox textBox_bing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ h1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        private delegate void MyDelegate();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task task_baidu = new Task(() => { Search_baidu("https://www.baidu.com/baidu?wd=" + textBox1.Text); }) ;
+            Task task_bing = new Task(() => { Search_bing("https://cn.bing.com/search?form=MOZTSB&pc=MOZI&q=" + textBox1.Text); });
+            task_baidu.Start();
+            task_bing.Start();
+        }
+        private void Search_baidu(string url)           // baidu
+        {
+            WebClient webClient = new WebClient();
+            byte[] recvdata = webClient.DownloadData(url);       // 指定url下载数据到byte数组中
+            string response = Encoding.UTF8.GetString(recvdata); // 获取UTF-8类型编码
+            this.BeginInvoke(new MyDelegate(() => { textBox_baidu.Text = response.Substring(0, 100); }));   // 将转换后的数据填入textBox
+        }
+        private void Search_bing(string url)           // bing
+        {
+            WebClient webClient = new WebClient();
+            byte[] recvdata = webClient.DownloadData(url);
+            string response = Encoding.UTF8.GetString(recvdata);
+            this.BeginInvoke(new MyDelegate(() => { textBox_bing.Text = response.Substring(0, 100); }));
+        }
+    }
+}
